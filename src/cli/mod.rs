3a807c08fbf5f7adcc72851e62087a601ed09e76
C <DeveloperC@protonmail.com>
2020-08-18 11:21:57 +0100

    refactor: CLI arguments/descriptions moved to own module

---
+++ src/cli/mod.rs
@@ -0,0 +1,24 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "conventional_commits_linter",
+    about = "A utility to lint Git commit messages against the Convectional Commits v1.0.0 format."
+)]
+pub struct Arguments {
+    #[structopt(
+        long = "from-commit-hash",
+        help = "The Git commit hash from where to take the range of commits from till HEAD to lint. The range is inclusive of HEAD and exclusive of the provided commit hash."
+    )]
+    pub from_commit_hash: Option<git2::Oid>,
+    #[structopt(
+        long = "from-tag",
+        help = "The Git tag from where to take the range of commits from till HEAD to lint. The range is inclusive of HEAD and exclusive of the provided tag."
+    )]
+    pub from_tag: Option<String>,
+    #[structopt(
+        long = "allow-angular-type-only",
+        help = "Allow the Conventional Commits type to only be (`build`, `ci`, `docs`, `feat`, `fix`, `perf`, `refactor`, `style`, `test`, `revert`), otherwise linting for the commit will fail."
+    )]
+    pub allow_angular_type_only: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -7,53 +7,31 @@ extern crate regex;
 
 use structopt::StructOpt;
 
+mod cli;
 mod git;
 mod linter;
 
-#[derive(Debug, StructOpt)]
-#[structopt(
-    name = "conventional_commits_linter",
-    about = "A utility to lint Git commit messages against the Convectional Commits v1.0.0 format."
-)]
-struct Args {
-    #[structopt(
-        long = "from-commit-hash",
-        help = "The Git commit hash from where to take the range of commits from till HEAD to lint. The range is inclusive of HEAD and exclusive of the provided commit hash."
-    )]
-    from_commit_hash: Option<git2::Oid>,
-    #[structopt(
-        long = "from-tag",
-        help = "The Git tag from where to take the range of commits from till HEAD to lint. The range is inclusive of HEAD and exclusive of the provided tag."
-    )]
-    from_tag: Option<String>,
-    #[structopt(
-        long = "allow-angular-type-only",
-        help = "Allow the Conventional Commits type to only be (`build`, `ci`, `docs`, `feat`, `fix`, `perf`, `refactor`, `style`, `test`, `revert`), otherwise linting for the commit will fail."
-    )]
-    allow_angular_type_only: bool,
-}
-
 fn main() {
     pretty_env_logger::init();
-    let args = Args::from_args();
-    debug!("The command line arguments provided are {:?}.", args);
+    let arguments = cli::Arguments::from_args();
+    debug!("The command line arguments provided are {:?}.", arguments);
 
-    if args.from_commit_hash.is_some() && args.from_tag.is_some() {
+    if arguments.from_commit_hash.is_some() && arguments.from_tag.is_some() {
         error!("Provide either the --from-tag or --from-commit-hash arguments not both.");
         std::process::exit(1);
     }
 
-    if args.from_commit_hash.is_none() && args.from_tag.is_none() {
+    if arguments.from_commit_hash.is_none() && arguments.from_tag.is_none() {
         error!("Provide either the --from-tag or --from-commit-hash argument.");
         std::process::exit(1);
     }
 
     let mut commit_messages: Vec<String> = vec![];
-    if let Some(oid) = args.from_commit_hash {
+    if let Some(oid) = arguments.from_commit_hash {
         commit_messages = git::get_commit_messages_from(oid)
     }
 
-    if let Some(tag_name) = args.from_tag {
+    if let Some(tag_name) = arguments.from_tag {
         match git::get_tag_oid(&tag_name) {
             Some(oid) => {
                 commit_messages = git::get_commit_messages_from(oid);
@@ -66,7 +44,7 @@ fn main() {
     }
 
     let number_of_linting_errors =
-        linter::lint_commits(commit_messages, args.allow_angular_type_only);
+        linter::lint_commits(commit_messages, arguments.allow_angular_type_only);
     if number_of_linting_errors > 0 {
         error!(
             "{} commits failed Conventional Commits v1.0.0 linting.",
