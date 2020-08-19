790170d3e4ae02d535d19d2b28c8b8d7bb4b97bf
C <DeveloperC@protonmail.com>
2020-08-19 19:11:21 +0000

    feat: improved summary format

--- src/reporter/mod.rs
+++ src/reporter/mod.rs
@@ -1,12 +1,15 @@
 use crate::model::LintingError;
+use console::Style;
 use git2::Oid;
 use std::collections::HashMap;
 
 pub fn print_summary(linting_errors: &HashMap<Oid, Vec<LintingError>>) {
     let number_of_issues: usize = linting_errors.values().map(|x| x.len()).sum();
+    let red = Style::new().red().bold();
 
     println!(
-        "Found {} issues across {} commits.",
+        "{} - Found {} seperate linting issues across {} commits.",
+        red.apply_to("X"),
         number_of_issues,
         linting_errors.len()
     );
