0bb172329f6273dec6bb2f215b6814c647804a2f
C <DeveloperC@protonmail.com>
2020-11-20 14:12:58 +0000

    test: Git env var feature tests

--- end-to-end-tests/features/steps/git.py
+++ end-to-end-tests/features/steps/git.py
@@ -30,3 +30,8 @@ def clone_remote_repository_and_checkout_commit(
 @given('the directory is changed to the cloned repository.')
 def change_into_git_dir(context):
     os.chdir(context.temporary_directory.name)
+
+
+@given('the GIT_DIR environment variable is set to the cloned repository.')
+def set_git_dir(context):
+    os.environ["GIT_DIR"] = str(context.temporary_directory.name + "/.git")
