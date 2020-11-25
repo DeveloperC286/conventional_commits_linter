991a6f0b94b50e74e4c21a804c0665d56859f0a1
C <DeveloperC@protonmail.com>
2020-11-25 23:03:17 +0000

    test: adding no space after type unit tests

---
+++ src/linter/conventional_commits_specification/no_space_after_type/tests.rs
@@ -0,0 +1,43 @@
+use rstest::rstest;
+
+use super::*;
+
+#[rstest(
+    commit_message,
+    case("feat:zsh auto completion (#1292) "),
+    case("chore(release):13.1.0"),
+    case("fix!(deps):Update os-locale to avoid security vulnerability (#1270)"),
+    case("fix:calling parse multiple times now appropriately maintains state (#\n\n")
+)]
+fn test_no_space_after_type(commit_message: &str) {
+    assert_eq!(lint(commit_message), Err(LintingError::NO_SPACE_AFTER_TYPE));
+}
+
+#[rstest(
+    commit_message,
+    case("  fix!(deps):Update os-locale to avoid security vulnerability (#1270)"),
+    case("\tfix:calling parse multiple times now appropriately maintains state (#\n\n")
+)]
+fn test_no_space_after_type_with_preceding_whitespace(commit_message: &str) {
+    assert_eq!(lint(commit_message), Err(LintingError::NO_SPACE_AFTER_TYPE));
+}
+
+#[rstest(
+    commit_message,
+    case("feat: zsh auto completion (#1292) "),
+    case("chore(release): 13.1.0"),
+    case("fix!(deps): Update os-locale to avoid security vulnerability (#1270)"),
+    case("fix: calling parse multiple times now appropriately maintains state (#\n\n")
+)]
+fn test_not_no_space_after_type(commit_message: &str) {
+    assert!(lint(commit_message).is_ok());
+}
+
+#[rstest(
+    commit_message,
+    case("GetHandle() -> GetProcess().Handle()\n"),
+    case("GetRenderProcessHost() has been removed for OOPI support, should use #include GetMainFrame()->GetProcess()\n")
+)]
+fn test_not_no_space_after_type_on_non_conventional_commit(commit_message: &str) {
+    assert!(lint(commit_message).is_ok());
+}
