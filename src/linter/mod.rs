31e9de961ca8d68525cfce04cb366bd02ad57350
DeveloperC <DeveloperC@protonmail.com>
2021-07-14 21:54:03 +0100

    refactor: separating commit lint method

--- src/linter/mod.rs
+++ src/linter/mod.rs
@@ -15,54 +15,7 @@ pub fn lint_commits(
     let mut oid_to_linting_errors = HashMap::new();
 
     for commit in commits {
-        let mut linting_errors = vec![];
-
-        match conventional_commits_specification::lint(&commit.message) {
-            Ok(()) => {}
-            Err(linting_error) => {
-                linting_errors.push(linting_error);
-
-                match conventional_commits_specification::empty_scope::lint(&commit.message) {
-                    Ok(()) => {}
-                    Err(linting_error) => {
-                        linting_errors.push(linting_error);
-                    }
-                }
-
-                match conventional_commits_specification::preceding_whitespace::lint(
-                    &commit.message,
-                ) {
-                    Ok(()) => {}
-                    Err(linting_error) => {
-                        linting_errors.push(linting_error);
-                    }
-                }
-
-                match conventional_commits_specification::no_description::lint(&commit.message) {
-                    Ok(()) => {}
-                    Err(linting_error) => {
-                        linting_errors.push(linting_error);
-                    }
-                }
-
-                match conventional_commits_specification::no_space_after_type::lint(&commit.message)
-                {
-                    Ok(()) => {}
-                    Err(linting_error) => {
-                        linting_errors.push(linting_error);
-                    }
-                }
-            }
-        }
-
-        if allow_angular_type_only {
-            match allow_angular_type_only::lint(&commit.message) {
-                Ok(()) => {}
-                Err(linting_error) => {
-                    linting_errors.push(linting_error);
-                }
-            }
-        }
+        let linting_errors = lint_commit(commit, allow_angular_type_only);
 
         if !linting_errors.is_empty() {
             oid_to_linting_errors.insert(commit.oid, linting_errors);
@@ -70,6 +23,56 @@ pub fn lint_commits(
     }
 
     oid_to_linting_errors
+}
+
+pub fn lint_commit(commit: &Commit, allow_angular_type_only: bool) -> Vec<LintingError> {
+    let mut linting_errors = vec![];
+
+    match conventional_commits_specification::lint(&commit.message) {
+        Ok(()) => {}
+        Err(linting_error) => {
+            linting_errors.push(linting_error);
+
+            match conventional_commits_specification::empty_scope::lint(&commit.message) {
+                Ok(()) => {}
+                Err(linting_error) => {
+                    linting_errors.push(linting_error);
+                }
+            }
+
+            match conventional_commits_specification::preceding_whitespace::lint(&commit.message) {
+                Ok(()) => {}
+                Err(linting_error) => {
+                    linting_errors.push(linting_error);
+                }
+            }
+
+            match conventional_commits_specification::no_description::lint(&commit.message) {
+                Ok(()) => {}
+                Err(linting_error) => {
+                    linting_errors.push(linting_error);
+                }
+            }
+
+            match conventional_commits_specification::no_space_after_type::lint(&commit.message) {
+                Ok(()) => {}
+                Err(linting_error) => {
+                    linting_errors.push(linting_error);
+                }
+            }
+        }
+    }
+
+    if allow_angular_type_only {
+        match allow_angular_type_only::lint(&commit.message) {
+            Ok(()) => {}
+            Err(linting_error) => {
+                linting_errors.push(linting_error);
+            }
+        }
+    }
+
+    linting_errors
 }
 
 #[cfg(test)]
