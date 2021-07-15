f6f6be052319ca063090c49999fa96fbc5b2d8de
DeveloperC <DeveloperC@protonmail.com>
2021-07-15 01:13:22 +0100

    fix: non angular ignores preceding whitespace and scope

--- src/linter/allow_angular_type_only/mod.rs
+++ src/linter/allow_angular_type_only/mod.rs
@@ -5,8 +5,11 @@ use crate::model::LintingError;
 pub fn lint(commit_message: &str) -> Result<(), LintingError> {
     lazy_static! {
         static ref ANGULAR_TYPE_REGEX: Regex = Regex::new(&format!(
-            r"(?i)^{}(\([[:alpha:]]*\))?{}:",
+            r"(?i)^{}{}{}{}{}:",
+            *crate::linter::regex::OPTIONAL_PRECEDING_WHITESPACE,
             crate::linter::regex::ANGULAR_TYPE,
+            crate::linter::regex::OPTIONAL_EXCLAMATION,
+            crate::linter::regex::OPTIONAL_EMPTY_SCOPE_OR_SCOPE,
             crate::linter::regex::OPTIONAL_EXCLAMATION,
         ))
         .unwrap();
--- src/linter/tests/generated_tests/mod.rs
+++ src/linter/tests/generated_tests/mod.rs
@@ -66,3 +66,66 @@ fn test_angular_type_commits_with_no_angular_type_only_assertion() {
         }
     }
 }
+
+#[test]
+fn test_non_angular_type_commits_with_angular_type_only_assertion() {
+    let number_of_variants: usize = 4;
+    let allow_angular_type_only = true;
+
+    for i in 1..2_usize.pow(number_of_variants as u32) {
+        //Given
+        let binary_string = format!(
+            "{:0desired_length$b}",
+            i,
+            desired_length = number_of_variants
+        );
+
+        let (commits, mut expected_linting_errors) = generation::generate_non_angular_type_commits(
+            utilities::is_position_in_binary_string_true(&binary_string, 0),
+            utilities::is_position_in_binary_string_true(&binary_string, 1),
+            utilities::is_position_in_binary_string_true(&binary_string, 2),
+            utilities::is_position_in_binary_string_true(&binary_string, 3),
+        );
+        expected_linting_errors.push(LintingError::NonAngularType);
+
+        //When/Then
+        for commit in commits {
+            assert_linting_errors_eq!(
+                expected_linting_errors,
+                lint_commit(&commit, allow_angular_type_only),
+                commit.message
+            );
+        }
+    }
+}
+
+#[test]
+fn test_angular_type_commits_with_angular_type_only_assertion() {
+    let number_of_variants: usize = 4;
+    let allow_angular_type_only = true;
+
+    for i in 1..2_usize.pow(number_of_variants as u32) {
+        //Given
+        let binary_string = format!(
+            "{:0desired_length$b}",
+            i,
+            desired_length = number_of_variants
+        );
+
+        let (commits, expected_linting_errors) = generation::generate_angular_type_commits(
+            utilities::is_position_in_binary_string_true(&binary_string, 0),
+            utilities::is_position_in_binary_string_true(&binary_string, 1),
+            utilities::is_position_in_binary_string_true(&binary_string, 2),
+            utilities::is_position_in_binary_string_true(&binary_string, 3),
+        );
+
+        //When/Then
+        for commit in commits {
+            assert_linting_errors_eq!(
+                expected_linting_errors,
+                lint_commit(&commit, allow_angular_type_only),
+                commit.message
+            );
+        }
+    }
+}
