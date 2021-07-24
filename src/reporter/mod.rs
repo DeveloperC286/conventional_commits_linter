b7a20295a02b5f76bd9f4f7c775c0676148aa60f
DeveloperC <DeveloperC@protonmail.com>
2021-07-24 20:35:30 +0100

    refactor: converting to ansi_term

--- src/reporter/mod.rs
+++ src/reporter/mod.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use console::Style;
+use ansi_term::Colour::Red;
 use git2::Oid;
 
 use crate::model::{Commit, LintingError};
@@ -11,52 +11,52 @@ pub fn pretty_print_linting_error(
     linting_errors: &[LintingError],
 ) -> String {
     let mut pretty_print = String::new();
-    let red = Style::new().red().bold();
+    let red = Red.bold();
 
     if let Some(oid) = oid {
-        pretty_print.push_str(&format!("{} - {}\n", red.apply_to("Commit Hash"), oid));
+        pretty_print.push_str(&format!("{} - {}\n", red.paint("Commit Hash"), oid));
     }
-    pretty_print.push_str(&format!("{} - {:?}\n", red.apply_to("Message"), message));
+    pretty_print.push_str(&format!("{} - {:?}\n", red.paint("Message"), message));
 
     if linting_errors.contains(&LintingError::NonConventionalCommitsSpecification) {
         pretty_print.push_str(&format!(
            "\t{} - Commit title does not comply with the Conventional Commits V1.0.0 specification.\n",
-           red.apply_to("X")
+           red.paint("X")
        ));
     }
 
     if linting_errors.contains(&LintingError::PrecedingWhitespace) {
         pretty_print.push_str(&format!(
             "\t{} - Commit title has preceding whitespace characters.\n",
-            red.apply_to("X")
+            red.paint("X")
         ));
     }
 
     if linting_errors.contains(&LintingError::NonAngularType) {
         pretty_print.push_str(&format!(
             "\t{} - Commit title does not use an Angular type.\n",
-            red.apply_to("X")
+            red.paint("X")
         ));
     }
 
     if linting_errors.contains(&LintingError::EmptyScope) {
         pretty_print.push_str(&format!(
             "\t{} - Commit title has a scope which is empty.\n",
-            red.apply_to("X")
+            red.paint("X")
         ));
     }
 
     if linting_errors.contains(&LintingError::NoSpaceAfterColonPrecedingTypeAndScope) {
         pretty_print.push_str(&format!(
             "\t{} - Commit title has no space after the colon preceding the Conventional Commits type and scope.\n",
-            red.apply_to("X")
+            red.paint("X")
         ));
     }
 
     if linting_errors.contains(&LintingError::NoDescriptionAfterTypeAndScope) {
         pretty_print.push_str(&format!(
             "\t{} - Commit title has no description after the Conventional Commits type and scope.\n",
-            red.apply_to("X")
+            red.paint("X")
         ));
     }
 
@@ -80,12 +80,12 @@ pub fn pretty_print_linting_errors(
         }
     }
 
-    let red = Style::new().red().bold();
+    let red = Red.bold();
     let number_of_issues: usize = linting_errors.values().map(|x| x.len()).sum();
 
     pretty_print.push_str(&format!(
         "{} - Found {} separate linting issues across {} commits.",
-        red.apply_to("X"),
+        red.paint("X"),
         number_of_issues,
         linting_errors.len()
     ));
