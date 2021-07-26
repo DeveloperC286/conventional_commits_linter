4d057a689813a23963b28ebc1b2af8595f4d5114
DeveloperC <DeveloperC@protonmail.com>
2021-07-26 21:20:34 +0100

    refactor: improved JSON serialisation error handling

--- src/reporter/json/mod.rs
+++ src/reporter/json/mod.rs
@@ -1,26 +1,40 @@
 use std::collections::HashMap;
 
 use git2::Oid;
-
 use serde::Serialize;
+use std::process::exit;
 
 use crate::model::{Commit, LintingError};
 
+#[derive(Serialize)]
+struct CommitLintingErrorsJSON {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    commit_hash: Option<String>,
+    commit_message: String,
+    linting_errors: Vec<LintingError>,
+}
+
 pub fn print(message: &str, linting_errors: &[LintingError]) -> String {
-    serde_json::to_string(&[LintingErrorJSON {
+    match serde_json::to_string(&[CommitLintingErrorsJSON {
         commit_hash: None,
         commit_message: message.to_string(),
         linting_errors: linting_errors.to_vec(),
-    }])
-    .unwrap()
+    }]) {
+        Ok(json) => json,
+        Err(error) => {
+            error!("{:?}", error);
+            error!("Unable to serialize a Commit's Linting Errors to JSON.");
+            exit(crate::ERROR_EXIT_CODE);
+        }
+    }
 }
 
 pub fn print_all(commits: &[Commit], linting_errors: &HashMap<Oid, Vec<LintingError>>) -> String {
-    let mut linting_errors_json: Vec<LintingErrorJSON> = vec![];
+    let mut linting_errors_json: Vec<CommitLintingErrorsJSON> = vec![];
 
     for commit in commits {
         if let Some(linting_errors) = linting_errors.get(&commit.oid) {
-            linting_errors_json.push(LintingErrorJSON {
+            linting_errors_json.push(CommitLintingErrorsJSON {
                 commit_hash: Some(commit.oid.to_string()),
                 commit_message: commit.message.clone(),
                 linting_errors: linting_errors.clone(),
@@ -28,15 +42,14 @@ pub fn print_all(commits: &[Commit], linting_errors: &HashMap<Oid, Vec<LintingEr
         }
     }
 
-    serde_json::to_string(&linting_errors_json).unwrap()
-}
-
-#[derive(Serialize)]
-struct LintingErrorJSON {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    commit_hash: Option<String>,
-    commit_message: String,
-    linting_errors: Vec<LintingError>,
+    match serde_json::to_string(&linting_errors_json) {
+        Ok(json) => json,
+        Err(error) => {
+            error!("{:?}", error);
+            error!("Unable to serialize every Commit's Linting Errors to a JSON array.");
+            exit(crate::ERROR_EXIT_CODE);
+        }
+    }
 }
 
 #[cfg(test)]
