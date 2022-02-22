1b3ee58597b155187aee7618000ce7432c2a2a33
DeveloperC <DeveloperC@protonmail.com>
2022-02-22 21:43:03 +0000

    refactor: abstracted away generated test logic

--- conventional_commits_linter_lib/src/commits/commit/tests/generated_tests/generation/mod.rs
+++ conventional_commits_linter_lib/src/commits/commit/tests/generated_tests/generation/mod.rs
@@ -2,73 +2,58 @@ use super::*;
 
 mod variations;
 
-pub(crate) fn generate_non_angular_type_commits(
-    should_generate_preceding_whitespace: bool,
-    should_generate_empty_scope: bool,
-    should_not_generate_space_after_type: bool,
-    should_not_generate_description: bool,
-) -> (Vec<String>, Vec<LintingError>) {
-    generate_commits_with_type(
-        should_generate_preceding_whitespace,
-        variations::NON_ANGULAR_COMMIT_TYPE_VARIATIONS,
-        should_generate_empty_scope,
-        should_not_generate_space_after_type,
-        should_not_generate_description,
-    )
+pub(super) fn generate_non_angular_type_commits() -> Vec<(Vec<String>, Vec<LintingError>)> {
+    generate_commit_messages(variations::NON_ANGULAR_COMMIT_TYPE_VARIATIONS)
 }
 
-pub(crate) fn generate_angular_type_commits(
-    should_generate_preceding_whitespace: bool,
-    should_generate_empty_scope: bool,
-    should_not_generate_space_after_type: bool,
-    should_not_generate_description: bool,
-) -> (Vec<String>, Vec<LintingError>) {
-    generate_commits_with_type(
-        should_generate_preceding_whitespace,
-        variations::ANGULAR_COMMIT_TYPE_VARIATIONS,
-        should_generate_empty_scope,
-        should_not_generate_space_after_type,
-        should_not_generate_description,
-    )
-}
-
-fn generate_commits_with_type(
-    should_generate_preceding_whitespace: bool,
-    commit_type_variations: &[&str],
-    should_generate_empty_scope: bool,
-    should_not_generate_space_after_type: bool,
-    should_not_generate_description: bool,
-) -> (Vec<String>, Vec<LintingError>) {
-    let mut linting_errors = vec![LintingError::NonConventionalCommitsSpecification];
-
-    let preceding_whitespace_variations = variations::get_preceding_whitespace_variations(
-        &mut linting_errors,
-        should_generate_preceding_whitespace,
-    );
-    let scope_variations =
-        variations::get_scope_variations(&mut linting_errors, should_generate_empty_scope);
-    let after_type_variation = variations::get_after_type_variation(
-        &mut linting_errors,
-        should_not_generate_space_after_type,
-    );
-    let description_variations = variations::get_description_variations(
-        &mut linting_errors,
-        should_not_generate_description,
-    );
-
-    (
-        generate_commit_messages(
-            preceding_whitespace_variations,
-            commit_type_variations,
-            scope_variations,
-            after_type_variation,
-            description_variations,
-        ),
-        linting_errors,
-    )
+pub(super) fn generate_angular_type_commits() -> Vec<(Vec<String>, Vec<LintingError>)> {
+    generate_commit_messages(variations::ANGULAR_COMMIT_TYPE_VARIATIONS)
 }
 
 fn generate_commit_messages(
+    commit_type_variations: &[&str],
+) -> Vec<(Vec<String>, Vec<LintingError>)> {
+    let mut commit_messages = vec![];
+    let number_of_variants: usize = 4;
+
+    for i in 1..2_usize.pow(number_of_variants as u32) {
+        let binary_string = format!("{:0number_of_variants$b}", i);
+
+        let mut linting_errors = vec![LintingError::NonConventionalCommitsSpecification];
+
+        let preceding_whitespace_variations = variations::get_preceding_whitespace_variations(
+            &mut linting_errors,
+            is_position_in_binary_string_true(&binary_string, 0),
+        );
+        let scope_variations = variations::get_scope_variations(
+            &mut linting_errors,
+            is_position_in_binary_string_true(&binary_string, 1),
+        );
+        let after_type_variation = variations::get_after_type_variation(
+            &mut linting_errors,
+            is_position_in_binary_string_true(&binary_string, 2),
+        );
+        let description_variations = variations::get_description_variations(
+            &mut linting_errors,
+            is_position_in_binary_string_true(&binary_string, 3),
+        );
+
+        commit_messages.push((
+            generate_commit_messages_from(
+                preceding_whitespace_variations,
+                commit_type_variations,
+                scope_variations,
+                after_type_variation,
+                description_variations,
+            ),
+            linting_errors,
+        ));
+    }
+
+    commit_messages
+}
+
+fn generate_commit_messages_from(
     preceding_whitespace_variations: &[&str],
     commit_type_variations: &[&str],
     scope_variations: &[&str],
@@ -81,14 +66,21 @@ fn generate_commit_messages(
         for commit_type in commit_type_variations {
             for scope in scope_variations {
                 for description in description_variations {
-                    commit_messages.push(format!(
-                        "{}{}{}:{}{}",
-                        preceding_whitespace, commit_type, scope, after_type_variation, description
-                    ));
+                    commit_messages.push(format!("{preceding_whitespace}{commit_type}{scope}:{after_type_variation}{description}"));
                 }
             }
         }
     }
 
     commit_messages
+}
+
+fn is_position_in_binary_string_true(binary_string: &str, position: usize) -> bool {
+    match binary_string.chars().nth(position).unwrap() {
+        '0' => false,
+        '1' => true,
+        _ => {
+            panic!("Should be either 0 or 1.");
+        }
+    }
 }
--- conventional_commits_linter_lib/src/commits/commit/tests/generated_tests/mod.rs
+++ conventional_commits_linter_lib/src/commits/commit/tests/generated_tests/mod.rs
@@ -3,29 +3,14 @@ use super::*;
 mod generation;
 #[macro_use]
 mod macros;
-mod utilities;
 
 #[test]
 fn test_non_angular_type_commits_with_no_angular_type_only_assertion() {
-    let number_of_variants: usize = 4;
     let allow_angular_type_only = false;
 
-    for i in 1..2_usize.pow(number_of_variants as u32) {
-        //Given
-        let binary_string = format!(
-            "{:0desired_length$b}",
-            i,
-            desired_length = number_of_variants
-        );
-
-        let (commit_messages, expected_linting_errors) =
-            generation::generate_non_angular_type_commits(
-                utilities::is_position_in_binary_string_true(&binary_string, 0),
-                utilities::is_position_in_binary_string_true(&binary_string, 1),
-                utilities::is_position_in_binary_string_true(&binary_string, 2),
-                utilities::is_position_in_binary_string_true(&binary_string, 3),
-            );
-
+    for (commit_messages, expected_linting_errors) in
+        generation::generate_non_angular_type_commits()
+    {
         for commit_message in commit_messages {
             // Given
             let commit = Commit::from_commit_message(&commit_message);
@@ -41,24 +26,9 @@ fn test_non_angular_type_commits_with_no_angular_type_only_assertion() {
 
 #[test]
 fn test_angular_type_commits_with_no_angular_type_only_assertion() {
-    let number_of_variants: usize = 4;
     let allow_angular_type_only = false;
 
-    for i in 1..2_usize.pow(number_of_variants as u32) {
-        //Given
-        let binary_string = format!(
-            "{:0desired_length$b}",
-            i,
-            desired_length = number_of_variants
-        );
-
-        let (commit_messages, expected_linting_errors) = generation::generate_angular_type_commits(
-            utilities::is_position_in_binary_string_true(&binary_string, 0),
-            utilities::is_position_in_binary_string_true(&binary_string, 1),
-            utilities::is_position_in_binary_string_true(&binary_string, 2),
-            utilities::is_position_in_binary_string_true(&binary_string, 3),
-        );
-
+    for (commit_messages, expected_linting_errors) in generation::generate_angular_type_commits() {
         for commit_message in commit_messages {
             // Given
             let commit = Commit::from_commit_message(&commit_message);
@@ -74,24 +44,11 @@ fn test_angular_type_commits_with_no_angular_type_only_assertion() {
 
 #[test]
 fn test_non_angular_type_commits_with_angular_type_only_assertion() {
-    let number_of_variants: usize = 4;
     let allow_angular_type_only = true;
 
-    for i in 1..2_usize.pow(number_of_variants as u32) {
-        //Given
-        let binary_string = format!(
-            "{:0desired_length$b}",
-            i,
-            desired_length = number_of_variants
-        );
-
-        let (commit_messages, mut expected_linting_errors) =
-            generation::generate_non_angular_type_commits(
-                utilities::is_position_in_binary_string_true(&binary_string, 0),
-                utilities::is_position_in_binary_string_true(&binary_string, 1),
-                utilities::is_position_in_binary_string_true(&binary_string, 2),
-                utilities::is_position_in_binary_string_true(&binary_string, 3),
-            );
+    for (commit_messages, mut expected_linting_errors) in
+        generation::generate_non_angular_type_commits()
+    {
         expected_linting_errors.push(LintingError::NonAngularType);
 
         for commit_message in commit_messages {
@@ -109,24 +66,9 @@ fn test_non_angular_type_commits_with_angular_type_only_assertion() {
 
 #[test]
 fn test_angular_type_commits_with_angular_type_only_assertion() {
-    let number_of_variants: usize = 4;
     let allow_angular_type_only = true;
 
-    for i in 1..2_usize.pow(number_of_variants as u32) {
-        //Given
-        let binary_string = format!(
-            "{:0desired_length$b}",
-            i,
-            desired_length = number_of_variants
-        );
-
-        let (commit_messages, expected_linting_errors) = generation::generate_angular_type_commits(
-            utilities::is_position_in_binary_string_true(&binary_string, 0),
-            utilities::is_position_in_binary_string_true(&binary_string, 1),
-            utilities::is_position_in_binary_string_true(&binary_string, 2),
-            utilities::is_position_in_binary_string_true(&binary_string, 3),
-        );
-
+    for (commit_messages, expected_linting_errors) in generation::generate_angular_type_commits() {
         for commit_message in commit_messages {
             // Given
             let commit = Commit::from_commit_message(&commit_message);
