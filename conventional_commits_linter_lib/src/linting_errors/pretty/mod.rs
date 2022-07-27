937febeeee7a2a28540a0d981844083eb50d55c8
DeveloperC <DeveloperC@protonmail.com>
2022-07-27 19:46:36 +0100

    style: applying clippy linitng suggestions

--- conventional_commits_linter_lib/src/linting_errors/pretty/mod.rs
+++ conventional_commits_linter_lib/src/linting_errors/pretty/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, VecDeque};
+use std::fmt::Write;
 
 use ansi_term::Colour::Red;
 
@@ -16,65 +17,71 @@ pub(crate) fn print_all(
         let red = Red.bold();
 
         if let Some(commit_hash) = &commit.hash {
-            pretty_print.push_str(&format!("{} - {commit_hash}\n", red.paint("Commit Hash")));
+            let _ = writeln!(pretty_print, "{} - {commit_hash}", red.paint("Commit Hash"));
         }
 
-        pretty_print.push_str(&format!(
-            "{} - {:?}\n",
+        let _ = writeln!(
+            pretty_print,
+            "{} - {:?}",
             red.paint("Message"),
             commit.message
-        ));
+        );
 
         if linting_errors.contains(&LintingError::NonConventionalCommitsSpecification) {
-            pretty_print.push_str(&format!(
-            "\t{} - Commit title does not comply with the Conventional Commits V1.0.0 specification.\n",
+            let _ = writeln!(pretty_print,
+            "\t{} - Commit title does not comply with the Conventional Commits V1.0.0 specification.",
             red.paint("X")
-        ));
+        );
         }
 
         if linting_errors.contains(&LintingError::PrecedingWhitespace) {
-            pretty_print.push_str(&format!(
-                "\t{} - Commit title has preceding whitespace characters.\n",
+            let _ = writeln!(
+                pretty_print,
+                "\t{} - Commit title has preceding whitespace characters.",
                 red.paint("X")
-            ));
+            );
         }
 
         if linting_errors.contains(&LintingError::NonAngularType) {
-            pretty_print.push_str(&format!(
-                "\t{} - Commit title does not use an Angular type.\n",
+            let _ = writeln!(
+                pretty_print,
+                "\t{} - Commit title does not use an Angular type.",
                 red.paint("X")
-            ));
+            );
         }
 
         if linting_errors.contains(&LintingError::ExclamationMarkBeforeScope) {
-            pretty_print.push_str(&format!(
-                "\t{} - Commit title has a exclamation mark before the scope.\n",
+            let _ = writeln!(
+                pretty_print,
+                "\t{} - Commit title has a exclamation mark before the scope.",
                 red.paint("X")
-            ));
+            );
         }
 
         if linting_errors.contains(&LintingError::EmptyScope) {
-            pretty_print.push_str(&format!(
-                "\t{} - Commit title has a scope which is empty.\n",
+            let _ = writeln!(
+                pretty_print,
+                "\t{} - Commit title has a scope which is empty.",
                 red.paint("X")
-            ));
+            );
         }
 
         if linting_errors.contains(&LintingError::NoSpaceAfterColonPrecedingTypeAndScope) {
-            pretty_print.push_str(&format!(
-                "\t{} - Commit title has no space after the colon preceding the type and scope.\n",
+            let _ = writeln!(
+                pretty_print,
+                "\t{} - Commit title has no space after the colon preceding the type and scope.",
                 red.paint("X")
-            ));
+            );
         }
 
         if linting_errors.contains(&LintingError::NoDescriptionAfterTypeAndScope) {
-            pretty_print.push_str(&format!(
-                "\t{} - Commit title has no description after the type and scope.\n",
+            let _ = writeln!(
+                pretty_print,
+                "\t{} - Commit title has no description after the type and scope.",
                 red.paint("X")
-            ));
+            );
         }
 
-        pretty_print.push('\n');
         pretty_print
     }
 
@@ -82,7 +89,7 @@ pub(crate) fn print_all(
 
     for commit in order.iter() {
         if let Some(errors) = errors.get(commit) {
-            pretty_print.push_str(&print(commit, errors));
+            let _ = writeln!(pretty_print, "{}", print(commit, errors));
         }
     }
 
@@ -90,11 +97,12 @@ pub(crate) fn print_all(
         let red = Red.bold();
         let total_linting_errors: usize = errors.values().map(|x| x.len()).sum();
 
-        pretty_print.push_str(&format!(
+        let _ = write!(
+            pretty_print,
             "{} - Found {total_linting_errors} separate linting errors across {} commits.",
             red.paint("X"),
             errors.len()
-        ));
+        );
     }
 
     pretty_print
